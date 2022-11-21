b445201718fcc9734f2773b1786b8ed8e52edac6
raihan <rishaon0@gmail.com>
2022-11-21 17:05:44 +0600

    header component style completed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["imgbox.com", "images2.imgbox.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
